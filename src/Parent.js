d1b77059b07991f0da84e85695194dde448b0996
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-09-22 23:48:48 +0300

    completed assgnment

--- src/Parent.js
+++ src/Parent.js
@@ -2,9 +2,18 @@ import React, { useState } from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 import Child from "./Child";
 
+
+
+
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
